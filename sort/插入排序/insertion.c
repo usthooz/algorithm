cfac1451532a6adddc13da204e78febdf9d413b8
swxctx <swxctxone@yeah.net>
2018-09-15 17:13:21 +0800

    增加C语言实现

---
+++ sort/插入排序/insertion.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#define LEN 5
+ 
+/*
+    input:
+    4 1 5 2 3
+    output:
+    1 2 3 4 5
+*/
+void insertion(int a[], int n);
+ 
+void insertion(int a[], int n)
+{
+    int i;
+    for (i=1;i<n;i++)
+        // 小于前一个元素
+        if (a[i] < a[i-1])
+        {
+            int index = i-1;
+            int temp = a[i];
+            // 找到合适的位置
+            while(index >=0 && a[index]>temp){
+                a[index+1] = a[index];
+                index --;
+            }
+            a[index+1] = temp;
+        }
+}
+ 
+int main()
+{
+    int number[LEN] = {4,1,5,2,3};
+    int i;
+    printf("input:\n");
+    for (i = 0; i < LEN; i++)
+    {
+        printf(" %d", number[i]);
+    }
+    insertion(number, LEN);
+    printf("\noutput:\n");
+    for (i = 0; i < LEN; i++)
+    {
+        printf(" %d", number[i]);
+    }
+}
